09c9864f2f54e4924e2e9c69e056bfaaf666aa3a
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-29 16:06:11 +0200

    fix modules source

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 module "ovh" {
-    source = "git@github.com/N0zz/infra-tfmod-ovh.git"
+    source = "git@github.com:N0zz/infra-tfmod-ovh.git"
+
     dns_records = var.dns_records
 }
 module "uptimerobot" {
-    source = "git@github.com/N0zz/infra-tfmod-uptimerobot.git"
+    source = "git@github.com:N0zz/infra-tfmod-uptimerobot.git"
     dns_records = var.dns_records
     uptimerobot_custom_monitors = var.custom_monitors
     uptimerobot_alert_email = var.uptimerobot_alert_email
