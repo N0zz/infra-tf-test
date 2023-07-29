a8df39bbef6930cc99bad09859ae0ca001dd406b
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-29 23:14:16 +0200

    nonsensitive

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "dns_records" {
-    value = module.ovh.dns_records
+    value = nonsensitive(module.ovh.dns_records)
 }
 output "alert_contact" {
-    value = module.uptimerobot.alert_contact
+    value = nonsensitive(module.uptimerobot.alert_contact)
 }
 output "https_monitors" {
-    value = module.uptimerobot.https_monitors
+    value = nonsensitive(module.uptimerobot.https_monitors)
 }
 output "custom_monitors" {
-    value = module.uptimerobot.custom_monitors
+    value = nonsensitive(module.uptimerobot.custom_monitors)
 }
 output "status_page" {
-    value = module.uptimerobot.status_page
+    value = nonsensitive(module.uptimerobot.status_page)
 }
