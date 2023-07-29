5dbc07f8d678a2a236c18665857a3a27a580335a
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-29 23:16:48 +0200

    try nonsensitive

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "dns_records" {
-    value = nonsensitive(module.ovh.dns_records)
+    value = nonsensitive(try(module.ovh.dns_records, ""))
 }
 output "alert_contact" {
-    value = nonsensitive(module.uptimerobot.alert_contact)
+    value = nonsensitive(try(module.uptimerobot.alert_contact, ""))
 }
 output "https_monitors" {
-    value = nonsensitive(module.uptimerobot.https_monitors)
+    value = nonsensitive(try(module.uptimerobot.https_monitors, ""))
 }
 output "custom_monitors" {
-    value = nonsensitive(module.uptimerobot.custom_monitors)
+    value = nonsensitive(try(module.uptimerobot.custom_monitors, ""))
 }
 output "status_page" {
-    value = nonsensitive(module.uptimerobot.status_page)
+    value = nonsensitive(try(module.uptimerobot.status_page, ""))
 }
