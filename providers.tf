e40d7182db4dda8632399063e6d18f8c6d64e718
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-29 16:14:06 +0200

    cleanup providers

--- providers.tf
+++ providers.tf
@@ -1,14 +1,2 @@
-provider "uptimerobot" {
-  api_key = var.uptimerobot_api_key
-}
-
-provider "ovh" {
-  endpoint           = var.ovh_endpoint
-  application_key    = var.ovh_application_key
-  application_secret = var.ovh_application_secret
-  consumer_key       = var.ovh_consumer_key
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
+provider "uptimerobot" {}
+provider "ovh" {}
