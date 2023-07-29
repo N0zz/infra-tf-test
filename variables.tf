5b401c9f56c09367f4d65bb796cafecb99e3f39a
Krzysztof Kolodziejczyk <kkolodziejczyk@gog.com>
2023-07-29 23:09:39 +0200

    test record

--- variables.tf
+++ variables.tf
@@ -15,6 +15,13 @@ variable "dns_records" {
             fieldtype = "A"
             ttl       = "300"
             target    = "130.61.181.137"
+        },
+        test2_kris_ovh = {
+            zone      = "kris.ovh"
+            subdomain = "test2"
+            fieldtype = "A"
+            ttl       = "300"
+            target    = "130.61.181.137"
         }
     }
 }
